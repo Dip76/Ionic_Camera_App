4b6a598ab1ad7cf7fc12540d4dbb52ec9869bf41
Dip <dip.patel@ordextechnology.com>
2024-01-22 11:03:44 +0530

    UI Changes
    -- added webview

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Browser } from '@capacitor/browser';
 
 @Component({
   selector: 'app-tab1',
@@ -8,5 +9,10 @@ import { Component } from '@angular/core';
 export class Tab1Page {
 
   constructor() {}
+
+  onViewClick() {
+    const linkToOpen = 'https://main--magnificent-eclair-cb873d.netlify.app/';
+    Browser.open({ url: linkToOpen });
+  }
 
 }
